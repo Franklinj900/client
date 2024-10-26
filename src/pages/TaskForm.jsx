eb2ec41c09ca71143ca3b83a1512288bf4da8c53
Franklin Ramos <Osegutierrez1607@gmail.com>
2024-10-26 18:59:56 -0400

    por fin luego de estar luchando logre hacer la conexion basica con el backend, logrando efectivamente crear el formulario para las tareas, revisar constantemente la conexion con el mongoDB, parece haber errores en ocasiones

--- src/pages/TaskForm.jsx
+++ src/pages/TaskForm.jsx
@@ -1,7 +1,35 @@
+import { useState } from "react";
+import axios from 'axios';
+
 function TaskForm() {
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('')
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const res = await axios.post('http://localhost:8000/api/tasks', {
+          title,
+          description
+        })
+        console.log(res);
+        e.target.reset();
+    }
+
     return (
-      <div>
-        <h1 className="text-3xl font-bold">TaskForm</h1>
+      <div className="flex justify-center items-center h-[calc(100vh-10rem)]">
+        <form className="bg-zinc-950 p-10" onSubmit={handleSubmit}>
+            <input type="text"
+             placeholder="title" 
+            className="block py-2 px-3 w-full mb-4 text-black rounded-md"
+            onChange={(e) => setTitle(e.target.value)}
+            />
+            <textarea 
+            placeholder="description"
+             rows="3"
+            className="block py-2 px-3 w-full mb-4 text-black rounded-md"
+            onChange={(e) => setDescription(e.target.value)}
+            ></textarea>
+            <button>Save</button>
+        </form>
       </div>
     )
   }
